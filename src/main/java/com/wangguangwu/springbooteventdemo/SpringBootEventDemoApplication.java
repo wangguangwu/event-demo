de358921587fcb0cd0149441f6cd2fb4619bb5e4
wangguangwu <wangguangwu@pukanghealth.com>
2024-12-03 20:36:10 +0800

    first commit

---
+++ src/main/java/com/wangguangwu/springbooteventdemo/SpringBootEventDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.springbooteventdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEventDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEventDemoApplication.class, args);
+	}
+
+}
